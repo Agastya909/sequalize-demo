a4eba5aa5f01024544af849bdd38b125dba41747
Agastya <agastyarajawat909@gmail.com>
2022-07-24 18:30:56 +0530

    remove timestamps

--- studentModel.js
+++ studentModel.js
@@ -1,24 +1,29 @@
 const { Sequelize, DataTypes, Model } = require("sequelize");
 const sequelize = require("./sequalize");
 
-const studentModel = sequelize.define("Student", {
-  UniqueID: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const studentModel = sequelize.define(
+  "Student",
+  {
+    UniqueID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Name: {
+      type: DataTypes.STRING,
+    },
+    Age: {
+      type: DataTypes.INTEGER,
+    },
+    Email: {
+      type: DataTypes.STRING,
+    },
   },
-  Name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Age: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  Email: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-});
+  {
+    timestamps: false,
+    createdAt: false,
+    updatedAt: false,
+  }
+);
 
 module.exports = studentModel;
